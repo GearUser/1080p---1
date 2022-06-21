844e1890514ff3280ebab83e0f39fbbc04031fb2
GearUser <105487991+GearUser@users.noreply.github.com>
2022-06-22 00:11:41 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ try:
     OWNER = config("OWNER")
     FFMPEG = config(
         "FFMPEG",
-        default='''ffmpeg -i "{}" -map 0:v -map 0:a -b:a 112k -c:a libopus -c:v libx265 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -crf 21 -c:s copy -map 0:s? -pix_fmt yuv420p -preset medium -profile:v main -vf smartblur=1.5:-0.35:-3.5:0.65:0.25:2.0,scale=1920:1080:spline16+accurate_rnd+full_chroma_int -x265-params me=2:rd=4:subme=7:aq-mode=3:aq-strength=1:deblock=1,1:psy-rd=1:psy-rdoq=1:rdoq-level=2:merange=57:bframes=8:b-adapt=2:limit-sao=1:frame-threads=3 "{}"''',
+        default='''ffmpeg -i "{}" -map 0:v -map 0:a -b:a 112k -c:a libopus -c:v libx265 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -crf 21 -c:s copy -map 0:s? -pix_fmt yuv420p10le -preset medium -profile:v main -vf smartblur=1.5:-0.35:-3.5:0.65:0.25:2.0,scale=1920:1080:spline16+accurate_rnd+full_chroma_int -x265-params me=2:rd=4:subme=7:aq-mode=3:aq-strength=1:deblock=1,1:psy-rd=1:psy-rdoq=1:rdoq-level=2:merange=57:bframes=8:b-adapt=2:limit-sao=1:frame-threads=3 "{}"''',
     )
     THUMB = config(
         "THUMBNAIL", default="www.google.com"
